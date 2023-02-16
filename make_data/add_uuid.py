4b8a4fcadc69702c1b91cbd8cf48ac30a155da32
tutntut <97011426+tutntut@users.noreply.github.com>
2023-02-16 11:21:01 +0900

    add annotation

--- make_data/add_uuid.py
+++ make_data/add_uuid.py
@@ -5,20 +5,20 @@ from utils.general import LOGGER
 
 
 def get_uuid(config, site):
-    headers = {'Content-Type': 'application/json', 'X-API-KEY':'o0808kcg8sosssoc84gkkos0sss8kcok4s4k40so'}  # api key는 바꿔주어야 될 수도 있음
-    # headers = {'Content-Type': 'application/json'}  # api key는 바꿔주어야 될 수도 있음
+    headers = {'Content-Type': 'application/json', 'X-API-KEY':'o0808kcg8sosssoc84gkkos0sss8kcok4s4k40so'}  # api key may have to change
+    # headers = {'Content-Type': 'application/json'}  # api key may have to change
     url = f'https://{site}.watchmile.com/api/v1/parking/slot/'
 
     get_pos = get_yaml(config)
 
-    # 서버에서 전체 주차면 상태 정보값 로딩
+    # load data from server
     slot_data = get_server(url, headers)
     total_state = slot_data.json()['lists']
     # with open('test.yaml', 'w', encoding='utf-8') as f:
     #     yaml.dump(total_state, f, default_style=False)
 
-    result = {}  # 결과값 저장
-
+    result = {}  # init result
+    
     for key, value in get_pos.items():
         result[key] = value
         roi = []
@@ -57,23 +57,23 @@ def get_uuid(config, site):
                     uuid_list.append(slot['uuid'])
                     break
 
-        #result['uuid'] = uuid_list
         result[key]['uuid'] = uuid_list
     return_yaml('result.yaml', result)
 
 def get_server(url: str, headers: str):
-    """get을 사용한 서버 통신 함수
+    """
+    Server communication functions using get
 
     Args:
-        url (str): 주차장 서버 주소
-        headers (str): 요청 헤더 값
+        url (str): Parking lot server address
+        headers (str): request header
     """
     try:
         r = requests.get(url, headers=headers)
-        if r.status_code != 200:  # 전송 실패시 상태 코드 출력
+        if r.status_code != 200:  # if fail show in LOGGER
             LOGGER.info("HTTP Error code: ", r.status_code)
         return r
-    # 오류 발생시 예외처리
+    # Error type
     except requests.exceptions.Timeout as errd:
         LOGGER.exception("Timeout Error : ", errd)
     except requests.exceptions.ConnectionError as errc:
@@ -95,4 +95,5 @@ def return_yaml(location: str, return_dict):
     with open(location, 'w') as f:
         yaml.dump(return_dict, f)
 
+# run
 get_uuid('add_ip.yaml', 'ansan-grancity-xi')
